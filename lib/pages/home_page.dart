84785eff3ca3001c06b26bcb26cb664bf23a668c
PanFei <fpan@che300.com>
2020-04-30 17:26:21 +0800

    修改文案

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("洛雪"),
+        child: Text("洛雪音乐"),
       ),
     );
   }
